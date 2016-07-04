c0096539a5787ae718eb699d500edcf25bdc3948
Elvis de Freitas Souza <edigitalb@gmail.com>
2016-07-04 11:21:29 -0300

    classpath otimizations
    Removing unnecessary dependencies, and otimize ram utilization

--- hostnameController.js
+++ hostnameController.js
@@ -1,13 +1,11 @@
 module.exports = function(app){
-	var http = require('http'), util = require('util');
 
 	app.get('/containers', (req, res) => {
-
+		var util = require('util');
 		app.data.containerEntries.forEach(c => {
 			res.write(util.format('container=%s, ip=%s, domain=%s', c.container, c.ip, c.domain));
 			res.write('\n');
 		});
 		res.end();
-
-	})
+	});
 }
