2d5fdbb1b71039d3f27606f9578cc805e57b7341
Elvis de Freitas <edigitalb@gmail.com>
2023-02-02 10:49:16 -0300

    Fixing intermitent test

--- src/test/java/com/mageddo/dnsproxyserver/config/ConfigsTest.java
+++ src/test/java/com/mageddo/dnsproxyserver/config/ConfigsTest.java
@@ -9,6 +9,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 
 import static com.mageddo.utils.TestUtils.readAndSortJson;
+import static com.mageddo.utils.TestUtils.readAndSortJsonExcluding;
 import static com.mageddo.utils.TestUtils.readAsStream;
 import static com.mageddo.utils.TestUtils.readString;
 import static com.mageddo.utils.TestUtils.sortJson;
@@ -30,11 +31,12 @@ class ConfigsTest {
     final var config = Configs.buildAndRegister(args);
 
     // assert
-    final var expectedJsonConfig = readAndSortJson("/configs-test/001.json");
-    assertEquals(expectedJsonConfig, sortJson(config));
+    assertEquals(
+      readAndSortJsonExcluding("/configs-test/001.json", "version"),
+      readAndSortJsonExcluding(config, "version")
+    );
     assertTrue(Files.exists(tmpConfigFile));
 
-    assertEquals(expectedJsonConfig, sortJson(config));
     assertEquals(sortJson(readString("/configs-test/002.json")), sortJson(readString(tmpConfigFile)));
   }
 
--- src/test/java/com/mageddo/utils/TestUtils.java
+++ src/test/java/com/mageddo/utils/TestUtils.java
@@ -3,6 +3,9 @@ package com.mageddo.utils;
 import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.mageddo.json.JsonUtils;
 import lombok.SneakyThrows;
 import lombok.experimental.UtilityClass;
 import org.apache.commons.io.IOUtils;
@@ -20,9 +23,12 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @UtilityClass
 public class TestUtils {
-  public static final ObjectMapper OBJECT_MAPPER = new ObjectMapper().enable(
-          MapperFeature.SORT_PROPERTIES_ALPHABETICALLY)
-      .enable(SerializationFeature.INDENT_OUTPUT);
+
+  public static final ObjectMapper objectMapper = JsonMapper.builder()
+    .enable(MapperFeature.SORT_PROPERTIES_ALPHABETICALLY)
+    .enable(SerializationFeature.ORDER_MAP_ENTRIES_BY_KEYS)
+    .enable(SerializationFeature.INDENT_OUTPUT)
+    .build();
 
   @SneakyThrows
   public static String readString(String path) {
@@ -30,20 +36,39 @@ public class TestUtils {
     assertNotNull(resource, "file not found: " + path);
     return IOUtils.toString(resource, "UTF-8");
   }
+
   @SneakyThrows
   public static String readAndSortJson(String path) {
     return sortJson(readString(path));
   }
 
+  public static String readAndSortJsonExcluding(Object o, String... excludingFields) {
+    final var tree = (ObjectNode) JsonUtils.instance().valueToTree(o);
+    for (String field : excludingFields) {
+      tree.remove(field);
+    }
+    return sortJson(tree);
+  }
+
   @SneakyThrows
-  public static String sortJson(String json) {
-    return OBJECT_MAPPER.writeValueAsString(OBJECT_MAPPER.readTree(json));
+  public static String readAndSortJsonExcluding(String path, String... excludingFields) {
+    final var tree = (ObjectNode) objectMapper.readTree(readString(path));
+    for (String field : excludingFields) {
+      tree.remove(field);
+    }
+    return sortJson(tree);
   }
 
   @SneakyThrows
   public static String sortJson(Object o) {
-    return sortJson(OBJECT_MAPPER.writeValueAsString(o));
+    return sortJson(objectMapper.writeValueAsString(o));
   }
+
+  @SneakyThrows
+  public static String sortJson(String json) {
+    return objectMapper.writeValueAsString(objectMapper.treeToValue(objectMapper.readTree(json), Object.class));
+  }
+
 
   @SneakyThrows
   public static InputStream readAsStream(String path) {
@@ -53,8 +78,8 @@ public class TestUtils {
   @SneakyThrows
   public static Path readResource(String path) {
     final var f = TestUtils.class
-        .getResource(path)
-        .getFile();
+      .getResource(path)
+      .getFile();
     return Paths.get(f);
   }
 
