408067cdd414c237ada62a8429114b50bf7e6446
unknown <edigitalb@gmail.com>
2023-02-24 19:59:43 -0300

    is not possible to create a singleton for different classloaders

--- src/main/java/com/mageddo/commons/lang/Singletons.java
+++ src/main/java/com/mageddo/commons/lang/Singletons.java
@@ -1,9 +1,13 @@
 package com.mageddo.commons.lang;
 
+import java.util.Map;
 import java.util.Properties;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Supplier;
 
 public class Singletons {
+
+  private static final Map<String, Object> store = new ConcurrentHashMap<>();
 
   private Singletons() {
   }
@@ -13,24 +17,28 @@ public class Singletons {
   }
 
   public static <T> T createOrGet(String key, Supplier<T> sup) {
-    // There should be just one system class loader object in the whole JVM.
-    synchronized (ClassLoader.getSystemClassLoader()) {
-      Properties sysProps = System.getProperties();
-      // The key is a String, because the .class object would be different across classloaders.
-      T cached = (T) sysProps.get(key);
-
-      // Some other class loader loaded JvmWideSingleton earlier.
-      if (cached != null) {
-        return cached;
-      } else {
-        // Otherwise this classloader is the first one, let's create a singleton.
-        // Make sure not to do any locking within this.
-        final var hotload = sup.get();
-        System.getProperties().put(key, hotload);
-        return hotload;
-      }
-    }
+    return (T) store.computeIfAbsent(key, _k -> sup.get());
   }
+
+//  public static <T> T createOrGet(String key, Supplier<T> sup) {
+//    // There should be just one system class loader object in the whole JVM.
+//    synchronized (ClassLoader.getSystemClassLoader()) {
+//      Properties sysProps = System.getProperties();
+//      // The key is a String, because the .class object would be different across classloaders.
+//      T cached = (T) sysProps.get(key);
+//
+//      // Some other class loader loaded JvmWideSingleton earlier.
+//      if (cached != null) {
+//        return cached;
+//      } else {
+//        // Otherwise this classloader is the first one, let's create a singleton.
+//        // Make sure not to do any locking within this.
+//        final var hotload = sup.get();
+//        System.getProperties().put(key, hotload);
+//        return hotload;
+//      }
+//    }
+//  }
 
   public static <T> T get(Class<?> clazz) {
     return get(clazz.getName());
