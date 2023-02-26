29a964eee4805c4f605fa4fe9b50f5ae1ba9cd16
Elvis de Freitas <edigitalb@gmail.com>
2023-02-25 21:02:33 -0300

    fixed system restart

--- src/main/java/com/mageddo/dnsproxyserver/systemd/ResolvedService.java
+++ src/main/java/com/mageddo/dnsproxyserver/systemd/ResolvedService.java
@@ -5,7 +5,7 @@ import org.apache.commons.lang3.Validate;
 
 public class ResolvedService {
   public static void restart() {
-    final var result = CommandLines.exec("service", "systemd-resolved", "restart");
+    final var result = CommandLines.exec("service systemd-resolved restart");
     Validate.isTrue(
       result.getExitCode() == 0,
       "Not possible to restart resolved service: %d : %s",
