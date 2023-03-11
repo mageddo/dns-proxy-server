d7687076e9b8e6483ded3a9e819d95a5b40fd0c9
Elvis de Freitas <edigitalb@gmail.com>
2023-03-10 21:00:38 -0300

    testing

--- src/test/java/com/mageddo/dnsproxyserver/server/rest/HostnameResourceCompTest.java
+++ src/test/java/com/mageddo/dnsproxyserver/server/rest/HostnameResourceCompTest.java
@@ -1,5 +1,44 @@
 package com.mageddo.dnsproxyserver.server.rest;
 
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import javax.ws.rs.core.Response;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.equalTo;
+
+@QuarkusTest
 class HostnameResourceCompTest {
+
+//  @BeforeAll
+//  static void beforeAll(){
+//    WebServer.start();
+//  }
+//
+//  @AfterAll
+//  static void afterAll(){
+//    WebServer.stop();
+//  }
+
+  @Test
+  void mustFindHostnamesButHasNoResult(){
+    // arrange
+
+    // act
+    final var response = given()
+      .queryParam("env", "")
+      .get("/hostname/find")
+      .then()
+      .log()
+      .ifValidationFails();
+
+    // assert
+    response
+      .statusCode(Response.Status.OK.getStatusCode())
+      .body(equalTo("[]"))
+      .log()
+    ;
+  }
 
 }
