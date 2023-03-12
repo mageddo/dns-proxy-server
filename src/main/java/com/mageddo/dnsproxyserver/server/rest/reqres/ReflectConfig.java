fc18e746beedbde763c9187d2500c66ad0373615
Elvis de Freitas <edigitalb@gmail.com>
2023-03-12 11:30:43 -0300

    fixing native image

---
+++ src/main/java/com/mageddo/dnsproxyserver/server/rest/reqres/ReflectConfig.java
@@ -0,0 +1,10 @@
+package com.mageddo.dnsproxyserver.server.rest.reqres;
+
+import nativeimage.Reflection;
+
+@Reflection(
+  scanPackage = "com.mageddo.dnsproxyserver.server.rest.reqres",
+  publicConstructors = true, declaredFields = true, publicMethods = true
+)
+public class ReflectConfig {
+}
