5c69a4cf45a64be690728d419255a2a8e2908909
Elvis de Freitas <edigitalb@gmail.com>
2023-03-12 11:34:57 -0300

    refactoring reflection config

--- src/main/java/com/mageddo/dnsproxyserver/server/rest/reqres/package-info.java
+++ src/main/java/com/mageddo/dnsproxyserver/server/rest/reqres/package-info.java
@@ -1,10 +1,7 @@
-package com.mageddo.dnsproxyserver.server.rest.reqres;
-
-import nativeimage.Reflection;
-
 @Reflection(
   scanPackage = "com.mageddo.dnsproxyserver.server.rest.reqres",
   publicConstructors = true, declaredFields = true, publicMethods = true
 )
-public class ReflectConfig {
-}
+package com.mageddo.dnsproxyserver.server.rest.reqres;
+
+import nativeimage.Reflection;
