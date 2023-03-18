1213e00cbbaa5eeeb25edd4f59443f4a5562de9a
Elvis de Freitas <edigitalb@gmail.com>
2023-03-18 18:46:15 -0300

    fixing test

--- src/test/java/com/mageddo/dnsproxyserver/server/dns/solver/SolverDockerTest.java
+++ src/test/java/com/mageddo/dnsproxyserver/server/dns/solver/SolverDockerTest.java
@@ -1,26 +1,26 @@
 package com.mageddo.dnsproxyserver.server.dns.solver;
 
-import com.mageddo.dnsproxyserver.docker.DockerDAO;
 import com.mageddo.dnsproxyserver.docker.ContainerSolvingService;
-import com.mageddo.dnsproxyserver.server.dns.Hostname;
+import com.mageddo.dnsproxyserver.docker.DockerDAO;
 import com.mageddo.dnsproxyserver.templates.HostnameTemplates;
 import com.mageddo.dnsproxyserver.templates.MessageTemplates;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
-import org.mockito.Mock.Strictness;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 class SolverDockerTest {
 
-  @Mock(strictness = Strictness.WARN)
+  @Mock
+    //(strictness = Strictness.WARN)
   ContainerSolvingService containerSolvingService;
 
   @Mock
@@ -39,7 +39,7 @@ class SolverDockerTest {
 
     final var query = MessageTemplates.acmeAQuery();
     final var ip = "0.0.0.0";
-    final var hostname = Hostname.of(HostnameTemplates.ACME_HOSTNAME);
+    final var hostname = HostnameQuery.ofWildcard(HostnameTemplates.ACME_HOSTNAME);
 
     doReturn(true)
       .when(this.dockerDAO)
@@ -57,31 +57,7 @@ class SolverDockerTest {
 
     final var resText = res.toString();
     assertTrue(resText.contains(ip), resText);
+    verify(this.containerSolvingService).findBestHostIP(hostname);
   }
 
-  @Test
-  void mustSolveWildcardHostname() {
-    // arrange
-
-    final var query = MessageTemplates.acmeAQuery();
-    final var ip = "0.0.0.0";
-    final var hostname = Hostname.of("." + HostnameTemplates.ACME_HOSTNAME);
-
-    doReturn(true)
-      .when(this.dockerDAO)
-      .isConnected()
-    ;
-    doReturn(ip)
-      .when(this.containerSolvingService)
-      .findBestHostIP(eq(hostname));
-
-    // act
-    final var res = this.solver.handle(query);
-
-    // assert
-    assertNotNull(res);
-
-    final var resText = res.toString();
-    assertTrue(resText.contains(ip), resText);
-  }
 }
