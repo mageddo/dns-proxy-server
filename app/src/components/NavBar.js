17aa8db2175e2b972401a51590bd27631375e029
Elvis de Freitas <edigitalb@gmail.com>
2023-03-17 23:36:48 -0300

    setup navbar

--- app/src/components/NavBar.js
+++ app/src/components/NavBar.js
@@ -1,25 +1,22 @@
 import React from 'react';
 
 const NavBar = () => (
-	<nav className="navbar navbar-inverse navbar-fixed-top" >
+	<nav className="navbar navbar-expand-lg navbar-dark bg-dark navbar-inverse navbar-fixed-top" >
 		<a className="navbar-brand" href="#">DNS Proxy Server</a>
 		<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
 			aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
 			<span className="navbar-toggler-icon"></span>
 		</button>
 		<div className="collapse navbar-collapse" id="navbarNav">
-			<ul className="navbar-nav pull-right">
+			<ul className="navbar-nav mr-auto pull-right">
 				<li className="nav-item active">
 					<a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
 				</li>
 				<li className="nav-item">
-					<a className="nav-link" href="#">Features</a>
+					<a className="nav-link" href="#">Settings</a>
 				</li>
 				<li className="nav-item">
-					<a className="nav-link" href="#">Pricing</a>
-				</li>
-				<li className="nav-item">
-					<a className="nav-link disabled" href="#">Disabled</a>
+					<a className="nav-link" href="http://mageddo.github.io/dns-proxy-server/latest/en/2-features/">Features / Docs</a>
 				</li>
 			</ul>
 		</div>
