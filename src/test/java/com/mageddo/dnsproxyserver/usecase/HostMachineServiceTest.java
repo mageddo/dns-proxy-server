8fbcd8919a9799ee900e8726f9f9074529df3930
Elvis de Freitas <edigitalb@gmail.com>
2023-03-20 22:22:48 -0300

    Fixing tests

---
+++ src/test/java/com/mageddo/dnsproxyserver/usecase/HostMachineServiceTest.java
@@ -0,0 +1,60 @@
+package com.mageddo.dnsproxyserver.usecase;
+
+import com.mageddo.dnsproxyserver.docker.DockerDAO;
+import com.mageddo.dnsproxyserver.templates.IpTemplates;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.doReturn;
+
+@ExtendWith(MockitoExtension.class)
+class HostMachineServiceTest {
+
+  @Mock
+  DockerDAO dockerDAO;
+
+  @Spy
+  @InjectMocks
+  HostMachineService machineService;
+
+  @Test
+  void mustReturnHostIP() {
+    // arrange
+    final var expectedIp = IpTemplates.local();
+    doReturn(expectedIp)
+      .when(this.machineService)
+      .findCurrentMachineIp()
+    ;
+
+    // act
+    final var ip = this.machineService.findHostMachineIP();
+
+    // assert
+    assertEquals(expectedIp, ip);
+  }
+
+  @Test
+  void mustReturnHostIPEvenWhenRunningInsideDockerContainer() {
+    // arrange
+    final var expectedIp = IpTemplates.local();
+    doReturn(expectedIp)
+      .when(this.dockerDAO)
+      .findHostMachineIp()
+    ;
+    doReturn(true)
+      .when(this.machineService)
+      .isDpsRunningInsideContainer()
+    ;
+
+    // act
+    final var ip = this.machineService.findHostMachineIP();
+
+    // assert
+    assertEquals(expectedIp, ip);
+  }
+}
