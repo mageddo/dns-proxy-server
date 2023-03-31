32ccce7aa8e8202c010ed702119863b9e88c8017
Elvis de Freitas <edigitalb@gmail.com>
2023-03-30 22:20:52 -0300

    must answer no error with no address when hostname is found but address don't

--- src/test/java/com/mageddo/dnsproxyserver/server/dns/solver/SolverSystemCompTest.java
+++ src/test/java/com/mageddo/dnsproxyserver/server/dns/solver/SolverSystemCompTest.java
@@ -1,6 +1,7 @@
 package com.mageddo.dnsproxyserver.server.dns.solver;
 
 import com.mageddo.dnsproxyserver.server.dns.Messages;
+import com.mageddo.dnsproxyserver.templates.HostnameTemplates;
 import com.mageddo.dnsproxyserver.usecase.HostMachineService;
 import com.mageddo.net.IP;
 import dagger.sheath.InjectMock;
@@ -15,6 +16,7 @@ import javax.inject.Inject;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
@@ -31,9 +33,9 @@ class SolverSystemCompTest {
   SolverSystem solver;
 
   @Test
-  void mustSolverHostMachineIp(){
+  void mustSolverHostMachineIp() {
     // arrange
-    final var hostname = "host.docker.";
+    final var hostname = HostnameTemplates.HOST_DOCKER;
     final var query = Messages.aQuestion(hostname);
 
     doReturn(IP.of("192.168.0.1"))
@@ -52,6 +54,21 @@ class SolverSystemCompTest {
     assertEquals("host.docker.\t\t30\tIN\tA\t192.168.0.1", answer);
 
     verify(this.machineService).findHostMachineIP(eq(IP.Version.IPV4));
+  }
+
+  @Test
+  void mustSolveNoErrorWhenHostIsFoundButAddressIsNot() {
+
+    // arrange
+    final var query = Messages.quadAQuestion(HostnameTemplates.HOST_DOCKER);
+
+    // act
+    final var res = this.solver.handle(query);
+
+    // assert
+    assertNotNull(res);
+    assertEquals("", Messages.detailedPrint(res.getMessage()));
+
   }
 
 }
