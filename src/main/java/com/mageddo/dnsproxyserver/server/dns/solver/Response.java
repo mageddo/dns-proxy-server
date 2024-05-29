ff440c50b17d55e4ea382af609924a0b311c5785
Elvis de Freitas <edigitalb@gmail.com>
2024-05-29 09:34:59 -0300

    documenting

--- src/main/java/com/mageddo/dnsproxyserver/server/dns/solver/Response.java
+++ src/main/java/com/mageddo/dnsproxyserver/server/dns/solver/Response.java
@@ -13,18 +13,26 @@ import java.time.LocalDateTime;
 @Builder(toBuilder = true)
 public class Response {
 
+  /**
+   * These magic numbers were defined as explained at #370.
+   * <br/>
+   * "I read it is a common approach to DNS Servers to cache names for more time than specified on the TTL
+   * In general people expects the server name address to be updated in some hours, then if DPS caches found
+   * hostnames for at least 5 minutes and the don't found for 1 hour, it might speed up a lot."
+   */
   public static final Duration DEFAULT_SUCCESS_TTL = Duration.ofMinutes(5);
   public static final Duration DEFAULT_NXDOMAIN_TTL = Duration.ofMinutes(60);
 
   /**
-   * The effective response.
+   * The effective response for the client.
    */
   @NonNull
   private Message message;
 
   /**
-   * The calculated TTL which will be answered to the client, can also be used to cache entries on DPS,
-   * it's not the same specified at {@link #message}. It is calculated
+   * The calculated TTL which will be used by the system to cache entries on DPS,
+   * it's not the same specified at {@link #message}. It is calculated with default values based on the scenario,
+   * see {@link #nxDomain(Message)} or {@link #success(Message)} for examples.
    */
   @NonNull
   private Duration ttl;
