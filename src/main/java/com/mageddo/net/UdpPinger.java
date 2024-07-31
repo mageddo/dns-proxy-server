cd276386d91d7bbeb4ace65a2afdf322b8426848
Elvis De Freitas Souza <edigitalb@gmail.com>
2024-07-31 15:24:57 -0300

    udp pinger

---
+++ src/main/java/com/mageddo/net/UdpPinger.java
@@ -0,0 +1,49 @@
+package com.mageddo.net;
+
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetSocketAddress;
+import java.time.Duration;
+
+/**
+ * credits to: https://github.com/awadalaa/Socket-Programming-Java/blob/5d07915640e337eb491ca11922ce19eb580c6bef/UDP-Pinger/PingClient.java
+ */
+public class UdpPinger {
+
+  public static boolean ping(InetSocketAddress address, Duration timeout) {
+    try {
+      sayHello(address, timeout);
+      return true;
+    } catch (UncheckedIOException e) {
+      return false;
+    }
+  }
+
+  public static byte[] sayHello(InetSocketAddress address, Duration timeout) {
+
+    final var pingDatagram = buildPingDatagram(address);
+
+    try (var socket = new DatagramSocket(address.getPort())) {
+
+      socket.setSoTimeout((int) timeout.toMillis());
+      socket.send(pingDatagram);
+
+      final var response = new DatagramPacket(new byte[1024], 1024);
+      socket.receive(response);
+      return response.getData();
+
+    } catch (IOException e) {
+      throw new UncheckedIOException(e);
+    }
+
+  }
+
+  private static DatagramPacket buildPingDatagram(InetSocketAddress address) {
+    final String pingStr = "PING " + System.nanoTime() + " \n";
+    final byte[] buf = pingStr.getBytes();
+    return new DatagramPacket(buf, buf.length, address);
+  }
+
+}
---
+++ src/test/java/com/mageddo/net/UdpPingerTest.java
@@ -0,0 +1,17 @@
+package com.mageddo.net;
+
+class UdpPingerTest {
+
+  public static void main(String[] args) {
+    try {
+//      ping(new String[]{"8.8.8.8", "53"});
+//      final var res = ping(IpAddrs.toInetSocketAddress(IpAddr.of("10.241.240.41", 53)));
+//      final var res = ping(IpAddrs.toInetSocketAddress(IpAddr.of("8.8.8.8", 53)));
+
+//      System.out.println(">>> " + res);
+    } catch (Exception e) {
+      System.out.println("Unknow error: " + e.getMessage());
+    }
+  }
+
+}
