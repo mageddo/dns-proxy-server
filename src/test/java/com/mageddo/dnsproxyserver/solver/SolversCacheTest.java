015378bfc09ec659ee14b60025f1008e10328119
Elvis de Freitas <edigitalb@gmail.com>
2024-07-30 23:39:25 -0300

    testing

--- src/test/java/com/mageddo/dnsproxyserver/solver/SolversCacheTest.java
+++ src/test/java/com/mageddo/dnsproxyserver/solver/SolversCacheTest.java
@@ -3,13 +3,19 @@ package com.mageddo.dnsproxyserver.solver;
 import com.mageddo.commons.concurrent.Threads;
 import com.mageddo.dns.utils.Messages;
 import com.mageddo.dnsproxyserver.solver.CacheName.Name;
+import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.xbill.DNS.Flags;
+import org.xbill.DNS.Message;
 import testing.templates.MessageTemplates;
 
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.concurrent.Callable;
+import java.util.concurrent.Executors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -21,8 +27,48 @@ class SolversCacheTest {
 
   SolverCache cache = new SolverCache(Name.GLOBAL);
 
+
   @Test
-  void mustCacheForTheSpecifiedTime(){
+  void mustLeadWithConcurrency() {
+
+    // arrange
+    final var req = MessageTemplates.acmeAQuery();
+    final var r = new Random();
+
+    // act
+    concurrentRequests(10_000, req, r);
+
+  }
+
+  @SneakyThrows
+  private void concurrentRequests(int quantity, Message req, Random r) {
+    final var runnables = new ArrayList<Callable<Object>>();
+    for (int i = 0; i < quantity; i++) {
+      runnables.add(() -> this.handleRequest(req, r));
+      if (i % 10 == 0) {
+        runnables.add(() -> {
+          this.cache.clear();
+          return null;
+        });
+      }
+    }
+
+    try (final var executor = Executors.newVirtualThreadPerTaskExecutor()) {
+      executor.invokeAll(runnables);
+    }
+  }
+
+  private Object handleRequest(Message req, Random r) {
+    this.cache.handleRes(req, message -> {
+      final var res = Response.internalSuccess(Messages.aAnswer(message, "0.0.0.0"));
+      Threads.sleep(r.nextInt(10));
+      return res;
+    });
+    return null;
+  }
+
+  @Test
+  void mustCacheForTheSpecifiedTime() {
 
     // arrange
     final var req = MessageTemplates.acmeAQuery();
@@ -45,7 +91,7 @@ class SolversCacheTest {
   }
 
   @Test
-  void mustCacheAndGetValidResponse(){
+  void mustCacheAndGetValidResponse() {
 
     // arrange
     final var req = MessageTemplates.acmeAQuery();
@@ -64,7 +110,7 @@ class SolversCacheTest {
   }
 
   @Test
-  void cantCacheWhenDelegateSolverHasNoAnswer(){
+  void cantCacheWhenDelegateSolverHasNoAnswer() {
     // arrange
     final var query = MessageTemplates.acmeAQuery();
 
