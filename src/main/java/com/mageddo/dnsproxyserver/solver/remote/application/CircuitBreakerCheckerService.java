035749f8af1b9712cfb22b8e1c34885ec8fce53a
Elvis de Freitas <edigitalb@gmail.com>
2024-08-01 13:31:54 -0300

    docs

--- src/main/java/com/mageddo/dnsproxyserver/solver/remote/application/CircuitBreakerCheckerService.java
+++ src/main/java/com/mageddo/dnsproxyserver/solver/remote/application/CircuitBreakerCheckerService.java
@@ -44,9 +44,8 @@ public class CircuitBreakerCheckerService {
   }
 
   /**
-   * // todo Ping isn't being effective.
+   * Note: Ping isn't being effective for DPS circuit breaker usage.
    * @see https://github.com/mageddo/dns-proxy-server/issues/526#issuecomment-2261421618
-   * @return
    */
   boolean ping(InetSocketAddress server) {
     return Networks.ping(server, SolverRemote.PING_TIMEOUT_IN_MS);
