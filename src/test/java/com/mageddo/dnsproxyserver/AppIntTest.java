9e19978216b5258bb31c03c36fccad1c52139775
Elvis de Freitas <edigitalb@gmail.com>
2024-08-05 18:14:58 -0300

    adjusting conf path

--- src/test/java/com/mageddo/dnsproxyserver/AppIntTest.java
+++ src/test/java/com/mageddo/dnsproxyserver/AppIntTest.java
@@ -7,24 +7,28 @@ import com.mageddo.dnsproxyserver.utils.Ips;
 import com.mageddo.net.SocketUtils;
 import com.mageddo.utils.Executors;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
 
 import java.io.IOException;
+import java.nio.file.Path;
 import java.time.Duration;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class AppIntTest {
   @Test
-  void appMustStart() throws IOException {
+  void appMustStart(@TempDir Path tmpPath) throws IOException {
 
     final var webServerPort = SocketUtils.findRandomFreePort();
     final var dnsServerPort = SocketUtils.findRandomFreePort();
+    final var configPath = tmpPath.resolve("config.json");
     final var hostToQuery = "dps-sample.dev";
 
     final var config = new String[]{
       "--default-dns=false",
       "--web-server-port=" + webServerPort,
       "--server-port=" + dnsServerPort,
+      "--conf-path=" + configPath,
     };
     final var app = new App(config);
 
