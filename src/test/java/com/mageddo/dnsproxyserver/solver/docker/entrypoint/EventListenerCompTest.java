6c78f41de50700a5d4d394bf6745c2e9c65849a1
Elvis de Freitas <edigitalb@gmail.com>
2024-08-06 18:24:36 -0300

    troubleshooting

--- src/test/java/com/mageddo/dnsproxyserver/solver/docker/entrypoint/EventListenerCompTest.java
+++ src/test/java/com/mageddo/dnsproxyserver/solver/docker/entrypoint/EventListenerCompTest.java
@@ -1,9 +1,12 @@
 package com.mageddo.dnsproxyserver.solver.docker.entrypoint;
 
+import ch.qos.logback.classic.Level;
 import com.mageddo.dnsproxyserver.di.Context;
 import com.mageddo.dnsproxyserver.di.StartupEvent;
 import com.mageddo.dnsproxyserver.di.StartupEvents;
+import com.mageddo.logback.LogbackUtils;
 import dagger.sheath.junit.DaggerTest;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
 import javax.inject.Inject;
@@ -11,6 +14,7 @@ import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@Slf4j
 @DaggerTest(component = Context.class)
 class EventListenerCompTest {
 
@@ -21,6 +25,8 @@ class EventListenerCompTest {
   void mustConfigureNetworkEventListener(){
 
     // arrange
+    LogbackUtils.changeLogLevel("com.mageddo", Level.TRACE);
+    log.debug("events={}", events);
 
     // act
     final var found = StartupEvents.exists(this.events, EventListener.class);
