52575225ca8e56f480b425f85da87cb50a77c20b
Elvis de Freitas <edigitalb@gmail.com>
2024-08-06 22:57:11 -0300

    reverting

--- src/test/java/com/mageddo/dnsproxyserver/solver/docker/entrypoint/EventListenerCompTest.java
+++ src/test/java/com/mageddo/dnsproxyserver/solver/docker/entrypoint/EventListenerCompTest.java
@@ -1,13 +1,9 @@
 package com.mageddo.dnsproxyserver.solver.docker.entrypoint;
 
-import ch.qos.logback.classic.Level;
 import com.mageddo.dnsproxyserver.di.Context;
 import com.mageddo.dnsproxyserver.di.StartupEvent;
 import com.mageddo.dnsproxyserver.di.StartupEvents;
-import com.mageddo.logback.LogbackUtils;
 import dagger.sheath.junit.DaggerTest;
-import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import javax.inject.Inject;
@@ -15,24 +11,16 @@ import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@Slf4j
 @DaggerTest(component = Context.class)
 class EventListenerCompTest {
 
   @Inject
   Set<StartupEvent> events;
 
-  @BeforeAll
-  static void beforeAll(){
-    LogbackUtils.changeLogLevel("dagger.sheath", Level.TRACE);
-    LogbackUtils.changeLogLevel("com.mageddo", Level.TRACE);
-  }
-
   @Test
   void mustConfigureNetworkEventListener(){
 
     // arrange
-    log.debug("events={}", events);
 
     // act
     final var found = StartupEvents.exists(this.events, EventListener.class);
