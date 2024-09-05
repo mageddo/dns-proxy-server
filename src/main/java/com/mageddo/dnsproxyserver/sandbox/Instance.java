9ac2c148871aeef5e67cf51faf4bc0d404389f6c
Elvis de Freitas <edigitalb@gmail.com>
2024-09-04 22:48:31 -0300

    Creating support for testing DPS really like a integration test, crating int test for solver remote happy pah

---
+++ src/main/java/com/mageddo/dnsproxyserver/sandbox/Instance.java
@@ -0,0 +1,52 @@
+package com.mageddo.dnsproxyserver.sandbox;
+
+import com.mageddo.commons.exec.Result;
+import com.mageddo.os.linux.kill.Kill;
+import lombok.Builder;
+import lombok.NonNull;
+import lombok.Value;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.Validate;
+
+@Slf4j
+@Value
+@Builder
+public class Instance {
+
+  @NonNull
+  Result result;
+
+  public static Instance of(Result result) {
+    return Instance.builder()
+      .result(result)
+      .build()
+      ;
+  }
+
+
+  public void sendHealthCheckSignal() {
+    this.sendHealthCheckSignalValidatingResult();
+    this.validateIsHealth();
+  }
+
+  private void validateIsHealth() {
+    final var out = this.getResult().getOutAsString();
+    final var isHealth = out.contains("dps.healthCheck.health=true");
+    Validate.isTrue(isHealth, "App not health yet, content=%s", out);
+  }
+
+  private void sendHealthCheckSignalValidatingResult() {
+    final var processId = this.getProcessId();
+    Validate.isTrue(processId != null, "Process not started yet");
+    log.trace("is alive: {}, exitcode={}", this.result.getProcess().isAlive(), this.result.getProcessExitCodeWhenAvailable());
+    Kill.sendSignal(10, processId);
+  }
+
+  private Long getProcessId() {
+    return this.result.getProcessId();
+  }
+
+  public void watchOutputInDaemonThread() {
+    getResult().printOutToLogsInBackground();
+  }
+}
