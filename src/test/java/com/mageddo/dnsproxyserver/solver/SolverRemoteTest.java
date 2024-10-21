fc8190918988140d8acdcb74d4bf63984e06a3ca
Elvis de Freitas <edigitalb@gmail.com>
2024-10-21 10:51:55 -0300

    fixing test

--- src/test/java/com/mageddo/dnsproxyserver/solver/SolverRemoteTest.java
+++ src/test/java/com/mageddo/dnsproxyserver/solver/SolverRemoteTest.java
@@ -17,34 +17,6 @@ class SolverRemoteTest {
   @InjectMocks
   SolverRemote solverRemote;
 
-//  @Test
-//  void mustPingRemoteServerWhileQueryingWhenFeatureIsActive(){
-//
-//    // arrange
-//    final var query = MessageTemplates.acmeAQuery();
-//    final var answer = MessageTemplates.buildAAnswer(query);
-//
-//    doReturn(true).when(this.solverRemote).isPingWhileGettingQueryResponseActive();
-//
-//    doReturn(ResolverTemplates.googleDnsAsList())
-//        .when(this.solverRemote)
-//        .findResolversToUse()
-//    ;
-//
-//    doReturn(CompletableFuture.completedFuture(answer))
-//        .when(this.solverRemote)
-//        .sendQueryAsyncToResolver(any());
-//
-//    this.excludeCircuitBreakerStrategyAndCallQueryMethodDirectly();
-//
-//    // act
-//    final var res = this.solverRemote.handle(query);
-//
-//    // assert
-//    assertNotNull(res);
-//    verify(this.solverRemote).pingWhileGettingQueryResponse(any(), any());
-//
-//  }
 //
 //  @Test
 //  void pingRemoteServerWhileQueryingDisabledByDefault(){
