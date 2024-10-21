4c428695b817b5514e5aae0b3cb725e09848eb02
Elvis de Freitas <edigitalb@gmail.com>
2024-10-21 10:53:00 -0300

    fixing test

--- src/test/java/com/mageddo/dnsproxyserver/solver/SolverRemoteTest.java
+++ src/test/java/com/mageddo/dnsproxyserver/solver/SolverRemoteTest.java
@@ -5,11 +5,6 @@ import org.mockito.InjectMocks;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.function.Supplier;
-
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doAnswer;
-
 @ExtendWith(MockitoExtension.class)
 class SolverRemoteTest {
 
@@ -17,22 +12,5 @@ class SolverRemoteTest {
   @InjectMocks
   SolverRemote solverRemote;
 
-//
-//  @Test
-//  void pingRemoteServerWhileQueryingDisabledByDefault(){
-//
-//    // act
-//    final var active = this.solverRemote.isPingWhileGettingQueryResponseActive();
-//
-//    // assert
-//    assertFalse(active);
-//
-//  }
 
-  void excludeCircuitBreakerStrategyAndCallQueryMethodDirectly() {
-    doAnswer(iom -> Supplier.class.cast(iom.getArgument(1)).get())
-      .when(this.solverRemote)
-      .queryUsingCircuitBreaker(any(), any())
-    ;
-  }
 }
