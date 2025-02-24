88e08a1bbee2c1a32273d8d79b69f45b8039da0a
Elvis de Freitas <edigitalb@gmail.com>
2025-02-24 11:20:39 -0300

    fixing test

--- src/main/java/com/mageddo/dnsproxyserver/config/SolverLocal.java
+++ src/main/java/com/mageddo/dnsproxyserver/config/SolverLocal.java
@@ -1,5 +1,6 @@
 package com.mageddo.dnsproxyserver.config;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Builder;
 import lombok.Value;
 
@@ -8,13 +9,15 @@ import java.util.List;
 @Value
 @Builder
 public class SolverLocal {
+
   private String activeEnv;
   private List<Config.Env> envs;
 
+  @JsonIgnore
   public Config.Env getFirst() {
     if (this.envs == null || this.envs.isEmpty()) {
       return null;
     }
-    return this.envs.get(0);
+    return this.envs.getFirst();
   }
 }
