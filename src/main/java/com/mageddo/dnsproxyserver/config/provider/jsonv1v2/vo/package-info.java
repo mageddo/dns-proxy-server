08a902b8b1071ce4bcd76366ad0242bfcf3ebd85
Elvis de Freitas <edigitalb@gmail.com>
2025-03-31 10:53:17 -0300

    fixing reflection config

--- src/main/java/com/mageddo/dnsproxyserver/config/provider/jsonv1v2/vo/package-info.java
+++ src/main/java/com/mageddo/dnsproxyserver/config/provider/jsonv1v2/vo/package-info.java
@@ -1,5 +1,5 @@
 @Reflection(
-  scanPackage = "com.mageddo.dnsproxyserver.config.jsonv1v2.vo",
+  scanPackage = "com.mageddo.dnsproxyserver.config.provider.jsonv1v2.vo",
   publicConstructors = true, constructors = true, declaredConstructors = true,
   publicMethods = true, declaredMethods = true,
   publicFields = true, declaredFields = true
