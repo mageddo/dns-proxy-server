bb34ebdddb0397012a5cb285498e97e073f25aed
Elvis de Freitas <edigitalb@gmail.com>
2025-12-16 19:26:44 -0300

    adjusts

--- src/test/java/com/mageddo/dnsproxyserver/config/dataformat/v3/mapper/ConfigV3JsonMapperTest.java
+++ src/test/java/com/mageddo/dnsproxyserver/config/dataformat/v3/mapper/ConfigV3JsonMapperTest.java
@@ -1,23 +1,21 @@
-package com.mageddo.dnsproxyserver.config.provider.dataformatv3.converter;
+package com.mageddo.dnsproxyserver.config.dataformat.v3.mapper;
 
 import com.mageddo.dnsproxyserver.config.provider.dataformatv3.templates.ConfigV3Templates;
-import com.mageddo.dnsproxyserver.config.dataformat.v3.dataprovider.JsonConfigDAO;
+
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class JsonConfigDAOTest {
-
-  JsonConfigDAO parser = new JsonConfigDAO();
+class ConfigV3JsonMapperTest {
 
   @Test
   void mustFindAndSerializeWithTheExactSameContent() {
 
     final var json = ConfigV3Templates.buildJson();
 
-    final var parsed = parser.parse(json);
-    final var marshalled = parser.serialize(parsed);
-    final var marshalledParsed = parser.parse(json);
+    final var parsed = ConfigV3JsonMapper.of(json);
+    final var marshalled = ConfigV3JsonMapper.toJson(parsed);
+    final var marshalledParsed = ConfigV3JsonMapper.of(json);
 
     assertEquals(json, marshalled);
     assertEquals(parsed, marshalledParsed);
