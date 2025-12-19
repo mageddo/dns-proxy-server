d4ad7de96aa266cb666a83cc4fb729e775813767
Elvis de Freitas <edigitalb@gmail.com>
2025-12-19 10:53:49 -0300

    adjusts

--- src/main/java/com/mageddo/dnsserver/UDPServerPool.java
+++ src/main/java/com/mageddo/dnsserver/UDPServerPool.java
@@ -1,14 +1,15 @@
 package com.mageddo.dnsserver;
 
+import java.net.InetSocketAddress;
 import java.net.SocketAddress;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
+import com.mageddo.commons.Collections;
 import com.mageddo.dnsproxyserver.utils.Ips;
 
 import lombok.RequiredArgsConstructor;
@@ -23,15 +24,21 @@ public class UDPServerPool {
   private List<UDPServer> servers = new ArrayList<>();
 
   public void start(int port) {
-    this.servers = Collections.singletonList(
-        new UDPServer(Ips.getAnyLocalAddress(port), this.requestHandler));
+    final var addresses = List.of(
+        Ips.getAnyLocalIpv6Address(port),
+        Ips.getAnyLocalAddress(port)
+    );
+    this.servers = Collections.map(
+        addresses, address -> new UDPServer(address, this.requestHandler)
+    );
     this.servers.forEach(UDPServer::start);
-    final var addresses = this.servers
-        .stream()
-        .map(UDPServer::getAddress)
+    log.info("Starting UDP server, addresses={}", this.toString(addresses));
+  }
+
+  private String toString(List<InetSocketAddress> addresses) {
+    return addresses.stream()
         .map(SocketAddress::toString)
         .collect(Collectors.joining(", "));
-    log.info("Starting UDP server, addresses={}", addresses);
   }
 
   public void stop() {
