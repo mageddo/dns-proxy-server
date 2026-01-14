4e1b674249be417725e4aa42a18c02c60115e49f
Elvis de Freitas <edigitalb@gmail.com>
2026-01-14 11:26:15 -0300

    adjusts

--- src/test/java/com/mageddo/dnsproxyserver/solver/docker/application/ContainerSolvingServiceTest.java
+++ src/test/java/com/mageddo/dnsproxyserver/solver/docker/application/ContainerSolvingServiceTest.java
@@ -254,7 +254,7 @@ class ContainerSolvingServiceTest {
     // assert
     assertNotNull(ip);
     assertTrue(ip.isHostnameMatched());
-    assertNull(ip.getIps());
+    assertTrue(ip.hasNotIP());
 
   }
 }
